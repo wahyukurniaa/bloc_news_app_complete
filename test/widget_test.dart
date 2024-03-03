1e4b5b863d18f69cdb6bc217fcbc4a4216c1cac7
wahyukurniaa <wahyukurnia321123@gmail.com>
2024-03-03 16:28:18 +0700

    y1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bloc_news_app_complete/main.dart';
+import 'package:bloc_newsapp_complete/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
